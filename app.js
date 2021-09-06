2f625c7592fa13bdb4032f1d821eb4d083fec10a
A298769 <mamarques@santandertecnologia.com.ar>
2021-09-06 20:23:09 -0300

    commit de heroku

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, ()=>{
-    console.log('Server up at http://localhost:3000');
+app.listen(PORT, ()=>{
+    console.log("Server up at http://localhost:" + PORT);
 });
 
 app.get('/', (req,res)=>{
